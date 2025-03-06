a48cd52974169065a8f9dca2c9e0dbf7bbdb3966
MoxshangBacancy <108790956+moxshang123@users.noreply.github.com>
2025-03-06 12:33:27 +0530

    Project setup using standard method

---
+++ EFCore/data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using EFCore.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCore.data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
